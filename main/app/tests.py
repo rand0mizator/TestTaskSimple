601f19532091fe368bdf47fa7b3a673cd52afcb4
Rand0mizator <iosuslov@gmail.com>
2021-10-05 16:32:29 +0700

    Добавлены тесты: response = status 200 url = 'home' template = 'index.html' page content = 'Выберите изображение:' Команда для тестирования: py manage.py test

--- main/app/tests.py
+++ main/app/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexPageTests(SimpleTestCase):
+
+    def test_index_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_home_page_contains_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Выберите изображение:')
+
+    def test_home_page_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(
+            response, 'Этого сообщения не должно быть на странице')
