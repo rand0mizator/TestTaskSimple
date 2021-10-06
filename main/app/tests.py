7dbd1b16763a352efbd7bc303edb17b4c22d94c4
Rand0mizator <iosuslov@gmail.com>
2021-10-06 11:27:21 +0700

    Добавлен тест подсчета пикселей: загружаем изображение красного квадрата, проверяем, что ответ соответстует. Команда для тестирования: py manage.py test

--- main/app/tests.py
+++ main/app/tests.py
@@ -1,5 +1,28 @@
 from django.test import SimpleTestCase
 from django.urls import reverse
+from PIL import Image
+from . import views
+from django.core.files.uploadedfile import SimpleUploadedFile
+import os
+
+
+class ImageTests(SimpleTestCase):
+    """Создаем тестовое изображение, загружаем на страницу и проверяем что количество найденных пикселей совпадает
+    после чего удаляем тестовое изображение.
+    """
+    def test_image_upload_and_count_pixels(self):
+        path = 'media\\test_image_100x100.png'
+        img = Image.new('RGB', (100, 100), color=(255, 0, 0))
+        img.save(path)
+        file = SimpleUploadedFile(name='test_image_100x100_u.png', content=open(path, 'rb').read(), content_type='image/jpeg')
+        color = 'ff0000'
+        data = {'image': img, 'color': color}
+        response = self.client.post('/', {'image': file, 'color': color})
+        self.assertContains(response, 'Количество пикселей (255, 0, 0) цвета: 10000')
+        self.assertContains(response, 'Белых (255, 255, 255) пикселей не найдено')
+        self.assertContains(response, 'Черных (0, 0, 0) пикселей не найдено')
+        os.remove(path)
+        os.remove('media\\test_image_100x100_u.png')
 
 
 class IndexPageTests(SimpleTestCase):
